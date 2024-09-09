57771e78812638cf0be78b4e952c9d9fe4fe485b
Mark Lester Caletina <caletina.marklester@gmail.com>
2024-09-09 10:14:52 +0800

    updating corrs

--- server/config/allowedOrigin.js
+++ server/config/allowedOrigin.js
@@ -1,13 +1,11 @@
 const allowedOrigins = [
     'http://localhost:3000/',
     'http://localhost:3000',
-    "https://dashboard-hpoi.vercel.app/",
-    "https://dashboard-hpoi.vercel.app",
-    "http://dashboard-hpoi.vercel.app/",
-    "http://dashboard-hpoi.vercel.app",
-    "https://dashboard-hpoi-lzej6f493-caletinamarklestergmailcoms-projects.vercel.app/"
-    "https://dashboard-hpoi.vercel.app/"
-    "dashboard-hpoi.vercel.app"
-]
+    'https://dashboard-hpoi.vercel.app/',
+    'https://dashboard-hpoi.vercel.app',
+    'http://dashboard-hpoi.vercel.app/',
+    'http://dashboard-hpoi.vercel.app',
+    'https://dashboard-hpoi-lzej6f493-caletinamarklestergmailcoms-projects.vercel.app/'
+];
 
 module.exports = allowedOrigins;
