9a5603e5240cbd303884fec892fc0b6290679ccc
Mark Lester Caletina <caletina.marklester@gmail.com>
2024-09-09 09:59:17 +0800

    adding server name

--- server/config/allowedOrigin.js
+++ server/config/allowedOrigin.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3000/',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    "https://dashboard-hpoi.vercel.app/"
 ]
 
 module.exports = allowedOrigins;
