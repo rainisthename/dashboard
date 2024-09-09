b3df954098ad6e6dbd0f00eed5e76c64c5598721
Mark Lester Caletina <caletina.marklester@gmail.com>
2024-09-09 10:07:19 +0800

    finalizing api

--- server/config/allowedOrigin.js
+++ server/config/allowedOrigin.js
@@ -1,7 +1,11 @@
 const allowedOrigins = [
     'http://localhost:3000/',
     'http://localhost:3000',
-    "https://dashboard-hpoi.vercel.app/"
+    "https://dashboard-hpoi.vercel.app/",
+    "https://dashboard-hpoi.vercel.app",
+    "http://dashboard-hpoi.vercel.app/",
+    "http://dashboard-hpoi.vercel.app",
+    "dashboard-hpoi.vercel.app"
 ]
 
 module.exports = allowedOrigins;
