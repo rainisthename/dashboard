2da848e0d50aa4aaf74dc20f512209987780cd30
Mark Lester Caletina <caletina.marklester@gmail.com>
2024-09-09 09:42:32 +0800

    latest commit

--- server/config/dbConnect.js
+++ server/config/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DATABASE_URI);
+        await mongoose.connect("mongodb+srv://caletinamarklester:R7tKYzcJ0K1d46PA@cluster0.3p8k4.mongodb.net/dashboard?retryWrites=true&w=majority");
     } catch (err) {
         console.error('MongoDB Connection Error:', err.message);
     }
